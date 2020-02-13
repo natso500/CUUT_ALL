66f7707308a170521e147df8861ade69a9505a29
natso500 <49106380+natso500@users.noreply.github.com>
2020-02-13 17:00:10 +0200

    Add files via upload

---
+++ CUUT_ALL.sh
@@ -0,0 +1,41 @@
+ #########color##############
+ B='\e[99;34m'
+ b='\e[99;39m'
+ N='\e[99;36m'
+ Y='\e[99;33m'
+ R='\e[99;31m'
+ G='\e[99;32m'
+ ####################
+ clear
+ figlet             "     CUUT_ALL  -F  " | lolcat -F 10
+echo -e $R
+echo                              "                                                               v2"
+ sleep 0.5
+echo -e $G
+ echo  "                   |         py NatSo 50          | "
+ sleep 0.5
+ echo  "                   |  instagram :  @s.1.aj        | "
+ sleep 0.5
+ echo  "                   | Email:natsonatso50@gmail.com | "
+ sleep 0.5
+ echo
+ sleep 0.5
+ echo -e " 1- >>  CUUT_ALL  <<  "
+ echo -e $R
+ sleep 0.5
+ echo -e " 2- >  Sweat devices connected to the router  < "
+ echo -e $N
+ sleep 0.5
+ read -p "Chose namber ? " sajed
+ sleep 0.5
+ if [ $sajed = 1 ];then
+ python2 Katsar.py 
+elif [ $sajed = 2 ];then
+ echo -n "Enter Your IP router: "
+ read g
+ clear
+ echo "                       ==== Connected Devices===="
+echo -e $G
+ nmap -sn --script=sniffer-detect $g/24
+ echo "========================================="
+ fi
